9bd9678147775a5a3aea6b03c215ea10ff72ba0c
donn-majidi <donn.majidi@gmail.com>
2026-05-17 11:04:07 +0200

    Init: Set up repository.

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+_version__ = "1.0.0"
+__all__ = ["__version__", "doc"]
+
+def doc() -> None:
+        import webbrowser
+        webbrowser.open("https://github.com/donn-majidi/arch-container-py/")
+
